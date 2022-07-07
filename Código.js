671accc0a736e2c796238c4979cf03cadae73e8b
JeffersonAlves7 <ilj031420@hotmail.com>
2022-07-07 16:42:26 -0300

    Nova função de retornar o Ean funcionando, função que será utilizada para cada edição dentro do projeto -> ela já está conseguindo separar os dados necessários para realizar o pedido e está se baseando nos mesmos para coletar outras informações, ean, total, etc e os jogando para outra planilha, com certeza esse processo de jogar para outra planilha pode melhorar

--- Código.js
+++ Código.js
@@ -1,62 +1,71 @@
 // FUNÇÕES SECUNDÁRIAS QUE SERÃO UTILIZADAS EM TODO O PROJETO POR OUTRAS FUNÇÕES -> PRINCIPAIS
+class SecundaryFunctions {
+    getUserSheet() {                                        //Pegando o nome do usuário para saber o index da planilha que está em uso
+        const userName = SpreadsheetApp.getActiveSheet().getName()
+        let indexSpreadsheet;
 
-function getUserSheet() {                                       //Pegando o nome do usuário para saber o index da planilha que está em uso
-    const userName = SpreadsheetApp.getActiveSheet().getName()
-    let indexSpreadsheet;
+        if (userName != "Yuri" && userName != "João") return false; //Retornando caso o nome não esteja nessa lista
 
-    if (userName != "Yuri" && userName != "João") return false; //Retornando caso o nome não esteja nessa lista
+        if (userName == "Yuri") indexSpreadsheet = 0;               //Coletando nome
+        if (userName == "João") indexSpreadsheet = 1;               //Coletando nome
 
-    if (userName == "Yuri") indexSpreadsheet = 0;               //Coletando nome
-    if (userName == "João") indexSpreadsheet = 1;               //Coletando nome
+        return indexSpreadsheet                                     //Retornando index do nome
+    }
+    apagarInformacoes(planilha, range) {                    //Passando dois parâmetos para apagar as informações
+        planilha.getRange(range).clearContent()                     //Simplesmente apagando o conteúdo passado
+    }
+    returnQntCaixa(referencia) {                            //A referência será uma string, nome de um produto cadastrado no sistema da empresa
+        const descricaoSheet = SpreadsheetApp.setActiveSheet(SpreadsheetApp.getActiveSpreadsheet().getSheets()[2])                  //Coletando a planilha descricao para uso
 
-    return indexSpreadsheet                                     //Retornando index do nome
-}
+        //Esta variável irá salvar a unica linha em que aparece a informação de codigo == referênia entregue
+        const descricao_data = descricaoSheet.getRange("A:A").getValues()                   //Coletando as informações de item da coluna A presente na planilha Descrição
+            .map((item, index) => ({ item: item, index: index + 1 }))                       //Mapeando os valores junto do index de suas linhas
+            .filter(({ item }) => item == referencia)                                       //Filtrando os itens para ver se as informações são iguais as entregadas pelo parâmetro da função
 
-function apagarInformacoes(planilha, range) {                   //Passando dois parâmetos para apagar as informações
-    planilha.getRange(range).clearContent()                     //Simplesmente apagando o conteúdo passado
-}
+        const quantidade = descricaoSheet.getRange("C" + descricao_data[0].index).getValue()
 
-function returnQntCaixa(referencia) {                           //A referência será uma string, nome de um produto cadastrado no sistema da empresa
-    const descricaoSheet = SpreadsheetApp.setActiveSheet(SpreadsheetApp.getActiveSpreadsheet().getSheets()[2])                  //Coletando a planilha descricao para uso
+        return quantidade                                           //A quantidade será um numero
+    }
+    returnEAN(referencia) {                                 //Retornarei o ean do item, para que em outras funções eu possa ver se o usuário realmente inseriu o ean correto para o item do checkout
+        const descricaoSheet = SpreadsheetApp.setActiveSheet(SpreadsheetApp.getActiveSpreadsheet().getSheets()[2])  //Coletando a planilha descricao para uso
 
-    //Esta variável irá salvar a unica linha em que aparece a informação de codigo == referênia entregue
-    const descricao_data = descricaoSheet.getRange("A:A").getValues()                   //Coletando as informações de item da coluna A presente na planilha Descrição
-        .map((item, index) => ({ item: item, index: index + 1 }))                       //Mapeando os valores junto do index de suas linhas
-        .filter(({ item }) => item == referencia)                                       //Filtrando os itens para ver se as informações são iguais as entregadas pelo parâmetro da função
+        //Esta variável irá salvar a unica linha em que aparece a informação de codigo == referênia entregue
+        const descricao_data = descricaoSheet.getRange("A:A").getValues()                   //Coletando as informações de item da coluna A presente na planilha Descrição
+            .map((item, index) => ({ item: item, index: index + 1 }))                       //Mapeando os valores junto do index de suas linhas
+            .filter(({ item }) => item == referencia)                                       //Filtrando os itens para ver se as informações são iguais as entregadas pelo parâmetro da função
 
-    const quantidade = descricaoSheet.getRange("C" + descricao_data[0].index).getValue()
+        const EAN = descricaoSheet.getRange("D" + descricao_data[0].index).getValue()
 
-    return quantidade                                           //A quantidade será um numero
-}
+        return EAN                                           //A quantidade será um numero
+    }
+    pegarTotalUNHistorico(numPedido, referencia) {          //Preciso passar os parâmetros para fazer a busca na planilha de histórico -> retorna o total de itens feito daquele pedido
+        const historicoSheet = SpreadsheetApp.setActiveSheet(SpreadsheetApp.getActiveSpreadsheet().getSheets()[4])                  //Coletando a planilha historico de pedidos para uso
 
-function pegarTotalUNHistorico(numPedido, referencia) {                        //Preciso passar os parâmetros para fazer a busca na planilha de histórico -> retorna o total de itens feito daquele pedido
-    const historicoSheet = SpreadsheetApp.setActiveSheet(SpreadsheetApp.getActiveSpreadsheet().getSheets()[4])                  //Coletando a planilha historico de pedidos para uso
+        //Esta variável irá salvar a unica linha em que aparece a informação de value == numPedido
+        const historico_numPedidos = historicoSheet.getRange("A:A").getValues()             //Coletando as informações de numPedido da coluna "A" da planilha Descrição
+            .map((num_pedido, index) => ({ num_pedido: num_pedido, index: index + 1 }))     //Mapeando os valores junto do index de suas linhas
+            .filter(({ num_pedido }) => num_pedido == numPedido)                            //Filtrando os itens para ver se as informações são iguais as entregadas pelo parâmetro da função
 
-    //Esta variável irá salvar a unica linha em que aparece a informação de value == numPedido
-    const historico_numPedidos = descricaoSheet.getRange("A:A").getValues()             //Coletando as informações de numPedido da coluna "A" da planilha Descrição
-        .map((num_pedido, index) => ({ num_pedido: num_pedido, index: index + 1 }))     //Mapeando os valores junto do index de suas linhas
-        .filter(({ num_pedido }) => num_pedido == numPedido)                            //Filtrando os itens para ver se as informações são iguais as entregadas pelo parâmetro da função
+        if (!historico_numPedidos[0]) return
+        let totalConferido;
 
-    if (!historico_numPedidos[0]) return
-    let totalConferido;
-
-    historico_numPedidos.forEach(({ numPedido, index }, i) => {
-        const data = historicoSheet.getRange("B" + index).getValue()
-        if (data == referencia) return
-        totalConferido = historicoSheet.getRange("G" + index).getValue()
-    })
-
-    return totalConferido
+        historico_numPedidos.forEach(({ numPedido, index }, i) => {
+            const data = historicoSheet.getRange("B" + index).getValue()
+            if (data == referencia) return
+            totalConferido = historicoSheet.getRange("G" + index).getValue()
+        })
+        return totalConferido
+    }
 }
 //FUNÇÕES PRINCIPAIS QUE SERÃO UTILIZADAS PELOS BOTÕES -> ESSAS FUNÇÕES UTILIZARÃO AS FUNÇÕES SECUNDÁRIAS
 
-function gerarPedido() {                                        //Gerar o pedido -> Coletar todas as informações necessárias para realizar o processo
-    const indexSpreadsheet = getUserSheet()                     //Coletando o index da planilha em que o usuário está -> só funciona se for seu nome
-    if (indexSpreadsheet === false) return;                     //Retornando caso o usuário não esteja na planilha referente ao seu nome
+function gerarPedido() {                                    //Gerar o pedido -> Coletar todas as informações necessárias para realizar o processo
+    const indexSpreadsheet = new SecundaryFunctions().getUserSheet()    //Coletando o index da planilha em que o usuário está -> só funciona se for seu nome
+    if (indexSpreadsheet === false) return;                             //Retornando caso o usuário não esteja na planilha referente ao seu nome
 
     const principalSheet = SpreadsheetApp.setActiveSheet(SpreadsheetApp.getActiveSpreadsheet().getSheets()[indexSpreadsheet])   //Selecionando a planilha referente ao usuário para uso principal dessa função
     const numPedido = principalSheet.getRange("A2").getValue()                                                                  //Coletando número do pedido
-    apagarInformacoes(principalSheet, "A6:F1000")                                                                               //Apagando as informações da planilha principal
+    new SecundaryFunctions().apagarInformacoes(principalSheet, "A6:I1000")                                                      //Apagando as informações da planilha principal
 
     const pedidosSheet = SpreadsheetApp.setActiveSheet(SpreadsheetApp.getActiveSpreadsheet().getSheets()[3])                    //Selecionando a planilha de pedidos
 
@@ -67,9 +76,9 @@ function gerarPedido() {                                        //Gerar o pedido
 
     const DATA = []                                             //Essa variável irá guardar algo parecido com isso = {Numero: "number", Referencia: "string"...}
 
-    for (let i = 0; i < pedidos_data.length; i++) {                                     //Essa repetição irá utilizar as colunas e as chaves que eu salvei acima
-        let columns = ["A", "B", "C", "D", "E"]                                         //Essa variável salva as colunas que serão utilizadas na hora de coletar as informações
-        const objectKeys = ["Numero", "Referencia", "Descricao", "UM", "Quant"]         //Essa variável guarda as chaves de cada uma das informações presentes na variável acima
+    for (let i = 0; i < pedidos_data.length; i++) {                                         //Essa repetição irá utilizar as colunas e as chaves que eu salvei acima
+        let columns = ["A", "B", "C", "D", "E"]                                             //Essa variável salva as colunas que serão utilizadas na hora de coletar as informações
+        const objectKeys = ["Numero", "Referencia", "Descricao", "UM", "Quant"]             //Essa variável guarda as chaves de cada uma das informações presentes na variável acima
 
         const obj = {}                                          //Essa constante irá receber as informações singulares, ela irá juntar as chaves com os valores
         objectKeys.forEach((key, j) => {                        //Essa repetição passará por cada coluna e por cada index da data
@@ -94,21 +103,74 @@ function gerarPedido() {                                        //Gerar o pedido
 }
 
 function apagar() {                                             //Função que apaga tudo o que está na visão do usuário -> essa funçãoseta a tabela do usuário para que funcione
-    const indexSpreadsheet = getUserSheet()                     //Coletando o index da planilha em que o usuário está -> só funciona se for seu nome
+    const indexSpreadsheet = new SecundaryFunctions().getUserSheet()  //Coletando o index da planilha em que o usuário está -> só funciona se for seu nome
     if (indexSpreadsheet === false) return;                     //Retornando caso o usuário não esteja na planilha referente ao seu nome
     const principalSheet = SpreadsheetApp.setActiveSheet(SpreadsheetApp.getActiveSpreadsheet().getSheets()[indexSpreadsheet])   //Selecionando a planilha referente ao usuário para uso principal dessa função
-    apagarInformacoes(principalSheet, "A6:F1000")                                                                               //Apagando as informações da planilha principal
+    new SecundaryFunctions().apagarInformacoes(principalSheet, "A6:I1000")                                                      //Apagando as informações da planilha principal
 }
 
-function doOnEdit() {
-    const indexSpreadsheet = getUserSheet()                     //Coletando o index da planilha em que o usuário está -> só funciona se for seu nome
+function doOnEdit() {                                           //Essa função será ativada sempre que ouver uma alteração em qualquer célula
+    const indexSpreadsheet = new SecundaryFunctions().getUserSheet()  //Coletando o index da planilha em que o usuário está -> só funciona se for seu nome
     if (indexSpreadsheet === false) return;                     //Retornando caso o usuário não esteja na planilha referente ao seu nome
 
+    //~~~~~~~~~~~ Dados referentes à célula ativa pelo usuário
     const active_range = SpreadsheetApp.getActiveRange()        //Pegando o range que está selecionado pelo usuário
-    const active_range_notation = active_range.getA1Notation()  //Pegando o endereço da célula
     const active_range_value = active_range.getValue()          //Pegando o valor selecionado na célula
 
-    if (active_range_notation[0] != "I") return                 // Retornando caso a coluna não seja a I
+    const active_range_row = active_range.getA1Notation()[1]    //Pegando Linha
+    const active_range_col = active_range.getA1Notation()[0]    //Pegando Coluna
 
-    console.log(active_range_value)
+    const principalSheet = SpreadsheetApp.setActiveSheet(SpreadsheetApp.getActiveSpreadsheet().getSheets()[indexSpreadsheet])   //Transferindo a planilha principal para uma variável caso precise em outros momentos
+    const postMessage = (value) => principalSheet.getRange("D2").setValue(value)            //Essa função irá adicionar uma mensagem na área de mensagens do usuário
+
+    postMessage("") //Resetar as mensagens sempre que iniciar
+    if (active_range_col != "I") return                         // Retornando caso a coluna não seja a I
+
+    //PASSO 1) Pegar todas as informações presentes na planilha do usuário que ativou a função
+    const row_numPedido = principalSheet.getRange("A" + active_range_row).getValue()        //Coletando o número do pedido presente na linha deste pedido
+    const row_referencia = principalSheet.getRange("B" + active_range_row).getValue()       //Coletando a referência do item
+    const row_totalConferido = principalSheet.getRange("G" + active_range_row).getValue()   //Coletando o total conferido
+    const row_totalItens = principalSheet.getRange("E" + active_range_row).getValue()
+    const row_tipo = principalSheet.getRange("H" + active_range_row).getValue()             //Coletando o tipo de dado que será bipado ["CX", "PC"]
+
+    if (row_totalConferido === row_totalItens) {                                            //Checando se o pedido já foi finalizado de acordo com o total de referências bipadas
+        postMessage("O pedido já foi finalizado")                                           //se sim ele retorna uma mensagem
+        return                                                                              //Retorna par não continuar a função
+    }
+
+    const row_EAN = new SecundaryFunctions().returnEAN(row_referencia)                      //Coletando o valor do EAN do item que está presente nessa linha do pedido
+
+
+    //PASSO 2) Pegar as informações que se encontram em outras planilhas
+    if (row_EAN != active_range_value) {
+        SpreadsheetApp.setActiveSheet(SpreadsheetApp.getActiveSpreadsheet().getSheets()[indexSpreadsheet])
+        postMessage("Insira o EAN corretamente")
+        return
+    };
+    console.log(row_EAN, active_range_value)
+
+    //PASSO 3) Verificar a quantidade de itens que estarão sendo checados a partir do tipo
+    let qntItem;
+    //~~~~~~~~~~~~ Funcionou [x] -> retornando 1 ou quantidade de itens em uma caixa
+    if (row_tipo == "CX") {                                         //Analizando se é um valor válido
+        qntItem = new SecundaryFunctions().returnQntCaixa(row_referencia)                   //Se for CX eu somo a quantidade de itens por caisa ao valor da qntItem
+    } else if (row_tipo == "PC") {
+        qntItem = 1                                                 //Se for PC eu somo a quantidade de itens com + 1
+    } else {                                                        //retornando caso não seja
+        postMessage("Insira um tipo de conferência na célula H5")
+        return
+    }
+
+    principalSheet.getRange("G" + active_range_row).setValue(Number(row_totalConferido) + 1)
+    // PASSO 4) Com a quantidade em mãos, devo selecionar a planilha de histórico e adicionar lá os dados referentes ao pedido
+    //~~~~~~~~~~~~ [] Verificar se essa mesma linha já existe no histórico, começando pelo número do pedido e depois pela referência do item
+    //~~~~~~~~~~~~ [] Se existir, somar o final
+    //~~~~~~~~~~~~ [] Se não existir, adicionar uma nova linha
+    const data = principalSheet.getRange(`A${active_range_row}:E${active_range_row}`).getValues()                                   //Separando as informações que que existem na planilha do usuário
+    data[0].push(Number(row_totalConferido) + 1)
+    console.log(data)
+    const historicoSheet = SpreadsheetApp.setActiveSheet(SpreadsheetApp.getActiveSpreadsheet().getSheets()[4])                      //Transferindo a planilha de histórico em uma variável
+    historicoSheet.getRange("A2:F2").setValues(data)
+    // FINAL) Retornar a planilha principal => planilha referente ao usuário que iniciou a função
+    SpreadsheetApp.setActiveSheet(SpreadsheetApp.getActiveSpreadsheet().getSheets()[indexSpreadsheet])
 }
--- README.md
+++ README.md
@@ -1,21 +1,22 @@
-07.06.2022
+07.07.2022
 <!-- Finalizado -->
+[X]  Função que observa alterações feitas na planilha do usuário →
+   [X] Essa função irá coletar a informação que o usuário bipou - EAN do item - E se ele realmente fizer parte daquela linha ele irá prosseguir
+   [X] Adicionar uma unidade ao total bipados - uma coluna da linha que está sendo utilizada
+   [X] Adicionar essa mesma linha na planilha de checkout → essa é a planilha que irá conter todas as informações dos pedidos e suas próprias referências
+   [X] Retornar para a planilha principal do usuário
+[X]  Função gerar pedido → 
+   [X] Coletar informações a partir do número de pedido que foi selecionado pelo usuário e o nome do usuário que fez essa busca
+   [X] Para cada index coletado, inserir os dados desse index na planilha do usuário que fez a alteração → Mais pra frente verificar a quantidade total de itens
+   [X] Adicionar também uma situação para cada pedido - **SITUAÇÕES:** “em aberto”, “finalizado”
+[X]  Função que apaga as informações que estão na tela
+   [X] Essa função irá apagar tudo que estiver na tela para dar a oportunidade de o usuário selecionar novamente outro número
+   [X] Ela será usada por outras funções, toda vez que for finalizado um pedido e toda vez que um novo pedido for gerado
 <!-- Em andamento -->
+
 <!-- Deve ser feito -->
-[ ]  Função gerar pedido → 
-   [ ] Coletar informações a partir do número de pedido que foi selecionado pelo usuário e o nome do usuário que fez essa busca
-   [ ] Para cada index coletado, inserir os dados desse index na planilha do usuário que fez a alteração → Mais pra frente verificar a quantidade total de itens
-    [ ] Adicionar também uma situação para cada pedido - **SITUAÇÕES:** “em aberto”, “finalizado”
-[ ]  Função que observa alterações feitas na planilha do usuário →
-   [ ] Essa função irá coletar a informação que o usuário bipou - EAN do item - E se ele realmente fizer parte daquela linha ele irá prosseguir
-   [ ] Adicionar uma unidade ao total bipados - uma coluna da linha que está sendo utilizada
-   [ ] Adicionar essa mesma linha na planilha de checkout → essa é a planilha que irá conter todas as informações dos pedidos e suas próprias referências
-   [ ] Retornar para a planilha principal do usuário
 [ ]  Função que envia todos os pedidos - ESTÁ INBUTIDA DENTRO DA FUNÇÃO QUE OBSERVA ALTERAÇÕES →
    [ ] Esse código precisa reconhecer todos os valores presentes na planilha do usuário, e com isso fazer uma verificação para perceber se o total de todos os itens é igual o total de itens que foram inseridos no checkout
    [ ] Se as duas informações forem idênticas, o código deve dizer ao usuário que o pedido foi finalizado em uma célula 
    [ ] E sugerir ao mesmo para que apague as informações que estão na tela dele
    [ ] Caso ele não opte por apagar as informações, deixar um botão que tem a mesma propriedade
-[ ]  Função que apaga as informações que estão na tela
-   [ ] Essa função irá apagar tudo que estiver na tela para dar a oportunidade de o usuário selecionar novamente outro número
-   [ ] Ela será usada por outras funções, toda vez que for finalizado um pedido e toda vez que um novo pedido for gerado
